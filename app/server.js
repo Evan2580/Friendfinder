0b5a8d11a47a7fb75aae8b1fb38335eb74ae38f4
unknown <evan2580@gmail.com>
2019-05-13 21:26:57 -0400

    server done, apiRoutes started and htmlRoutes

--- app/server.js
+++ app/server.js
@@ -1,1 +1,21 @@
+var express = require ('express');
+var path = require('path');
 
+var app = express();
+
+var PORT  = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Router points our api's and html
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+//LISTENER STARTS OUR SERVER
+
+app.listen(PORT, function(){
+  console.log("App listening on port: " + PORT);
+})
