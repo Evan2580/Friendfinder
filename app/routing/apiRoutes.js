863eeddb4ad0d00b172279878334cc5690d6587f
unknown <evan2580@gmail.com>
2019-05-17 21:47:59 -0400

    almost complete push up finished apiroutes, htmlroutes, home.html, friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,30 @@
+var friends = require("../data/friends");
 
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.json(friends)
+
+  })
+
+  app.post("/api/friends", function (req, res) {
+    var smallestDifference = 99999;
+    var bestMatch = {};
+    var user = req.body;
+    friends.forEach(friend => { // looping over friends array of objects
+
+      var totalDifference = 0;
+      for (var i = 0; i < friend.scores.length; i++) { //2nd loop over the scores
+        totalDifference += Math.abs(this.user.score[i] - friend.scores[i]);
+
+      }
+      if (totalDifference < smallestDifference) {
+        smallestDifference = totalDifference;
+        bestMatch = friend;
+      }
+    });
+    friends.push(user);
+    res.json(bestMatch);
+  })
+
+
+}
